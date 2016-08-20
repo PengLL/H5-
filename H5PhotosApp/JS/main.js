68e55d4d921c86b2c871e71d971dc31ba69c3660
PengL <373970862@qq.com>
2016-08-20 10:25:15 +0800

    实现了导航条的效果，并且修正了main.js的部分代码

--- H5PhotosApp/JS/main.js
+++ H5PhotosApp/JS/main.js
@@ -2,36 +2,36 @@ var $=jQuery.noConflict();
 var zWin=$(window);
 var large=$("#large");
 var largeImg=$("#largeImg");
-var totalImg=17;
 var cid;
+var totalImg=17;
 $(function () {
 	 FastClick.attach(document.body);
 });
 var mainObj={
-	init:function () {
+	init:function (folder,totalImg,location) {
 		var htmlTempl='';
 		var fontsize=$("html").css("font-size");
+		$("#"+folder).attr("data-count",totalImg);
 		for(var i=1;i<=totalImg;i++)
 		{
 			var marginLeft=0.1+'rem';
 			i%4==1 ? marginLeft=0+'rem' : marginLeft;
-			htmlTempl+="<li style='margin-left:"+marginLeft+"' class='animated bounceIn' data-id="+i+"><canvas id='cvs_"+i+"'></canvas></li>";
+			htmlTempl+="<li style='margin-left:"+marginLeft+"' class='animated bounceIn' data-id='"+i+"' data-folder='"+folder+"' ><canvas data-id='cvs"+folder+i+"'></canvas></li>";
 			var imgObj=new Image();
 			imgObj.index=i;
 			imgObj.onload=function () {
-				var cvs=$("#cvs_"+this.index)[0].getContext('2d');
-				var test=imgObj.width;
+				var cvs=$("[data-id='cvs"+folder+this.index+"']")[0].getContext('2d');
 				cvs.width=imgObj.width;
 				cvs.height=imgObj.height;
 				cvs.drawImage(this,0,0);
 			}
-			imgObj.src="Images/"+i+".jpg";
+			imgObj.src="Images/"+folder+"/"+i+".jpg";
 		}
-		$("#imgList").html(htmlTempl);
+		$(location+" .imgList").html(htmlTempl);
 	},
-	loadImage:function (id,callback) {
+	loadImage:function (id,callback,folder) {
 		var imgObj=new Image();
-		var imgsrc="Images/large"+id+".jpg";;
+		var imgsrc="Images/"+folder+"/large"+id+".jpg";;
 		imgObj.onload=function () {
 			var imgW=this.width;
 			var imgH=this.height;
@@ -46,7 +46,7 @@ var mainObj={
 				height:winH,
 			}).show();
 			largeImg.css("width","auto").css("height","auto");
-			largeImg.css("margin","0");
+			largeImg.css("margin","0").attr("data-folder",folder);
 			if (imgH/imgW>winH/winW) {
 				largeImg.attr("src",imgsrc).css("height",winH).css("width",relW).css("margin-left",paddingLeft);
 			}
@@ -59,13 +59,16 @@ var mainObj={
 		imgObj.src=imgsrc;
 	}
 }
-mainObj.init();
 
+mainObj.init("scenery",17,"#scenery");
+mainObj.init("sports",25,"#sports");
+mainObj.init("fresh",28,"#fresh");
 
 // 用委托添加点击每个小图的事件
-$("#container ul").delegate("li","click",function () {
+$(".container ul").delegate("li","click",function () {
 	var id=cid=$(this).attr("data-id");
-	mainObj.loadImage(id);
+	var folder=$(this).attr("data-folder");
+	mainObj.loadImage(id,"",folder);
 });
 
 
@@ -74,7 +77,6 @@ $("#largeImg").click(function () {
 	 $("#large").hide();
  });
 $("#largeImg").swipe({
-       
         swipe:function(event, direction, distance, duration, fingerCount) {
           if (direction=="left") {
           	cid==totalImg ? cid=totalImg :mainObj.loadImage(++cid,function () {
@@ -83,7 +85,7 @@ $("#largeImg").swipe({
 					largeImg[0].removeEventListener("webkitAnimationEnd");
 				});
 				$("#largeImg").addClass("animated bounceInRight");
-			});
+			},$(this).attr("data-folder"));
           }
           if (direction=="right") {
           	cid==1 ? cid=1: mainObj.loadImage(--cid,function () {
@@ -92,17 +94,26 @@ $("#largeImg").swipe({
 					largeImg[0].removeEventListener("webkitAnimationEnd");
 				});
 			 	$("#largeImg").addClass("animated bounceInLeft"); 
-		 	});
+		 	},$(this).attr("data-folder"));
           }  
       	},
       	threshold:30
-      });
-
+});
 // 导航条的切换
 $("header nav .item a").click(function () {
-	 $("header nav .item a").removeClass("active");
-	 $(this).addClass("active");
-})
+	var id=$(this).attr("href");
+	$(".item-container a").removeClass("active");
+	$(this).addClass("active");
+	$(".container").each(function () {
+		 if("#"+$(this).attr("id")==id){
+		 	$(this).show() ;
+		 	totalImg=$(this).attr("data-count");
+		 }
+		 else
+		   $(this).hide(); 
+	});
+
+});
 // 侧滑菜单
 $(".menu").click(function () {
 	  $(".mask").show();
